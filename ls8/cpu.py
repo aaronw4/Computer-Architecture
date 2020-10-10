cb6deeb6ad3a3719c80505d52818dd32622684ea
Aaron Williams <aaronrw4@yahoo.com>
2020-10-09 18:57:26 -0700

    Implemented JEQ, JNE, and JMP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,7 +8,8 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256
-        self.pc = 0
+        self.PC = 0
+        self.FL = 0b00000000
         self.reg = [0] * 8
         self.reg[7] = 0xF4
 
@@ -51,12 +52,12 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
+            self.PC,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram_read(self.PC),
+            self.ram_read(self.PC + 1),
+            self.ram_read(self.PC + 2)
         ), end='')
 
         for i in range(8):
@@ -77,58 +78,116 @@ class CPU:
         SP = self.reg[7]
 
         while running:
-            command = self.ram[self.pc]
+            command = self.ram[self.PC]
+
             # LDI
             if command == 0b10000010:
-                index = self.ram[self.pc + 1]
-                value = self.ram[self.pc + 2]
+                index = self.ram[self.PC + 1]
+                value = self.ram[self.PC + 2]
                 self.reg[index] = value
-                self.pc += 2
+                self.PC += 2
+
             # PRN
             elif command == 0b01000111:
-                index = self.ram[self.pc + 1]
+                index = self.ram[self.PC + 1]
                 print(self.reg[index])
-                self.pc += 1
+                self.PC += 1
+
             # MUL
             elif command == 0b10100010:
-                reg_index_1 = self.ram[self.pc + 1]
-                reg_index_2 = self.ram[self.pc + 2]
+                reg_index_1 = self.ram[self.PC + 1]
+                reg_index_2 = self.ram[self.PC + 2]
                 first_value = self.reg[reg_index_1]
                 second_value = self.reg[reg_index_2]
                 self.reg[reg_index_1] = first_value * second_value
-                self.pc += 2
+                self.PC += 2
+
             # PUSH
             elif command == 0b01000101:
                 SP -= 1
-                reg_index_1 = self.ram[self.pc + 1]
+                reg_index_1 = self.ram[self.PC + 1]
                 value = self.reg[reg_index_1]
                 self.ram[SP] = value
-                self.pc += 1
+                self.PC += 1
+
             # POP
             elif command == 0b01000110:
                 value = self.ram[SP]
-                reg_index_1 = self.ram[self.pc + 1]
+                reg_index_1 = self.ram[self.PC + 1]
                 self.reg[reg_index_1] = value
                 SP += 1
-                self.pc += 1
+                self.PC += 1
+
             # CALL
             elif command == 0b01010000:
-                return_address = self.pc + 2
+                return_address = self.PC + 2
                 SP -= 1
                 self.ram[SP] = return_address
-                reg_index_1 = self.ram[self.pc + 1]
+                reg_index_1 = self.ram[self.PC + 1]
                 subroutine_address = self.reg[reg_index_1]
-                self.pc = subroutine_address
+                self.PC = subroutine_address
+
             # RET
             elif command == 0b00010001:
                 return_address = self.ram[SP]
-                self.pc = return_address
+                self.PC = return_address
                 SP += 1
+
+            # CMP
+            elif command == 0b10100111:
+                reg_index_1 = self.ram[self.PC + 1]
+                reg_index_2 = self.ram[self.PC + 2]
+                value1 = self.reg[reg_index_1]
+                value2 = self.reg[reg_index_2]
+
+                if value1 < value2:
+                    self.FL = 0b00000100
+                elif value1 > value2:
+                    self.FL = 0b00000010
+                elif value1 == value2:
+                    self.FL = 0b00000001
+                
+                self.PC += 2
+
+            # JEQ
+            elif command == 0b01010101:
+                reg_index_1 =  self.ram[self.PC + 1]
+                address = self.reg[reg_index_1]
+
+                if self.FL == 0b00000001:
+                    self.PC = address
+                    continue
+                else:
+                    self.PC += 1
+
+            # JNE
+            elif command == 0b01010110:
+                reg_index_1 = self.ram[self.PC + 1]
+                address = self.reg[reg_index_1]
+                result = self.FL & 0b00000001
+
+                if result == 0:
+                    self.PC = address
+                    continue
+                else:
+                    self.PC += 1
+
+            # JMP
+            elif command == 0b01010100:
+                reg_index_1 = self.ram[self.PC + 1]
+                address = self.reg[reg_index_1]
+                self.PC = address
+                continue
+
             # HLT
             elif command == 0b00000001:
                 running = False
 
-            self.pc += 1
+            else:
+                print(f'unknown command: {command:b} on line {self.PC}!')
+                running = False
+
+            self.PC += 1
 
 if __name__ == '__main__':
     CPU = CPU()
